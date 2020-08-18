6168acf8766aa85f63aab601096948de9adbdff9
Yasser Bouhadi <bouhadi.yasser@gmail.com>
2020-08-18 15:32:16 +0200

    POM refactor v1.0

---
+++ FreeCRMTest/src/main/java/com/crm/qa/base/TestBase.java
@@ -0,0 +1,133 @@
+package com.crm.qa.base;
+
+import java.awt.AWTException;
+import java.awt.Robot;
+import java.awt.event.KeyEvent;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+
+import com.crm.qa.util.TestUtil;
+
+import io.github.bonigarcia.wdm.config.DriverManagerType;
+import io.github.bonigarcia.wdm.managers.ChromeDriverManager;
+
+public class TestBase {
+
+	public static WebDriver driver;
+	public static Properties prop;
+
+	By buttonAcceder = By.xpath("//*[@id=\"formularioPrincial\"]/div/div/div/div[3]/div/div/button");
+	By buttonAccess = By.xpath("//*[@id=\"tooltip1\"]/div[2]/div/fieldset/div");
+
+	public TestBase() {
+
+		try {
+			// Create Object of propreties class
+			prop = new Properties();
+			// Location of the Propreties File
+			FileInputStream ip = new FileInputStream(
+					"C:\\Development\\MCIN\\FreeCRMTest\\src\\main\\java\\com\\crm\\qa\\config\\config.properties");
+			// Connection between the Java Code and the Propreties File (Load Properties)
+			prop.load(ip);
+
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void initialization() {
+
+		String browserName = prop.getProperty("browser");
+
+		if (browserName.equals("chrome")) {
+			ChromeDriverManager.getInstance(DriverManagerType.CHROME).setup();
+			driver = new ChromeDriver();
+		}
+
+		else if (browserName.equals("FF")) {
+			ChromeDriverManager.getInstance(DriverManagerType.FIREFOX).setup();
+			driver = new FirefoxDriver();
+		}
+
+		else if (browserName.equals("IE")) {
+			ChromeDriverManager.getInstance(DriverManagerType.IEXPLORER).setup();
+			driver = new InternetExplorerDriver();
+		}
+
+		driver.manage().window().maximize();
+		driver.manage().deleteAllCookies();
+		driver.manage().timeouts().pageLoadTimeout(TestUtil.PAGE_LOAD_TIMEOUT, TimeUnit.SECONDS);
+		driver.manage().timeouts().implicitlyWait(TestUtil.IMPLICIT_WAIT, TimeUnit.SECONDS);
+
+		driver.get(prop.getProperty("url"));
+	}
+
+	public void sleep(Long time) {
+		if (time == null) {
+			time = 1000L;
+		}
+		try {
+			Thread.sleep(time);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void clickOnButtonAcceder() {
+		driver.findElement(buttonAcceder).click();
+
+//		driver.findElement(buttonAcceder).click();
+		sleep(null);
+	}
+
+	public void clickOnButtonAccess() {
+		driver.findElement(buttonAccess).click();
+//		driver.findElement(buttonAccess).click();
+		sleep(null);
+	}
+
+	public void chromeCertificate() {
+//		Runnable r = new Runnable() {
+//			@Override
+//			public void run() {
+//				try {
+//					clickOnButtonAccess();
+//				} catch (Exception e) {
+//					e.printStackTrace();
+//				}
+//			}
+//		};
+//
+		// Choose first certificate
+		try {
+			Robot robot = new Robot();
+			Thread t = new Thread();
+			t.start();
+			robot.delay(10000);
+			robot.keyPress(KeyEvent.VK_ENTER);
+			robot.keyRelease(KeyEvent.VK_ENTER);
+		} catch (AWTException e1) {
+			e1.printStackTrace();
+		}
+
+	}
+
+	public void authentication() throws AWTException {
+		clickOnButtonAcceder();
+		clickOnButtonAccess();
+		chromeCertificate();
+
+	}
+
+}
---
+++ FreeCRMTest/src/test/java/com/crm/qa/testcases/LoginPageTest.java
@@ -0,0 +1,44 @@
+package com.crm.qa.testcases;
+
+import java.awt.AWTException;
+
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import com.crm.qa.pages.LoginPage;
+
+public class LoginPageTest extends LoginPage {
+
+	@BeforeMethod
+	public void setUp() throws AWTException {
+		initialization();
+		authentication();
+
+		// Here we can add the authentification method
+	}
+
+	@Test(priority = 1)
+	public void loginPageTitleTest() {
+
+//		String title = loginPage.validateLoginPageTile();
+//		Assert.assertEquals(title, "Google");
+		// Use AssertEquals Assert.assertEquals(title, "Google", "Title not mached");
+	}
+
+//	@Test(priority = 2)
+//	public void crmLogoImageTest() {
+//		boolean flag = loginPage.validateCRMImage();
+//		Assert.assertTrue(flag);
+//	}
+//
+//	@Test(priority = 3)
+//	public void loginTest() {
+//		homePage = loginPage.login(prop.getProperty("username"), prop.getProperty("password"));
+//	}
+
+	@AfterMethod
+	public void tearDown() {
+		driver.quit();
+	}
+}
